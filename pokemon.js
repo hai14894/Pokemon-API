0c1cdd78b1d30f3f455f5c519008a182f975a358
hai14894 <hohoanghai14894@gmail.com>
2020-06-16 14:29:55 +1000

    add getpokemon function

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,14 @@
+async function getPokemon(name){
+    try{
+        const respond = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        return respond.data
+    }catch(e) {
+        e = "The pokemon you are looking for does not exist"
+        console.log(e);
+    }
+}   
+
 const searchBtn = document.querySelector("#search-btn")
 const searchBar = document.querySelector("#search-bar")
-
+getPokemon("pikachu")
 
